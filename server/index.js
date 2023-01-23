2007f0e07a6794d5204a0b1fae6c830ddf22f093
Ayan <xchjjvg07@gmail.com>
2023-01-23 07:28:56 +0600

    controllers logic added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const fileUpload = require('express-fileupload')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const path = require('path')
 const routes = require('./routes/routes.js')
 const errorHandling = require('./middleware/ErrorHandlingMiddleware')
 
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 9000
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, "static")))
+app.use(fileUpload({}))
 app.use('/api', routes)
 
 
